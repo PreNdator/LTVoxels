1047f916a8a84835d358d8d39efb24ad9a963371
Тим <ledenev.03@mail.ru>
2026-03-09 14:43:27 +0300

    Update VoxelChunkPathAttribute.cs

--- Assets/Scripts/Core/Attributes/VoxelChunkPathAttribute.cs
+++ Assets/Scripts/Core/Attributes/VoxelChunkPathAttribute.cs
@@ -2,6 +2,6 @@ namespace LedenevTV
 {
     public sealed class VoxelChunkPathAttribute : StreamingAssetsPathAttribute
     {
-        public VoxelChunkPathAttribute() : base(".ply", ".vox", ".bytes", ".txt") { }
+        public VoxelChunkPathAttribute() : base(".ply", ".vox", ".bytes", ".txt", ".bin") { }
     }
 }
